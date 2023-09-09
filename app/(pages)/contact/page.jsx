b8572b65296c92f419239b16f1a965ed9b560668
BarancanS <barancansng@gmail.com>
2023-09-09 22:21:33 +0300

    Update page.jsx

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -122,7 +122,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <div className="w-screen mt-10 mb-10">
+        <div className="w-full mx-auto py-10">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2493.6295923089533!2d21.425959884392913!3d41.99078763210152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135415cac5427391%3A0xa040ac67b6c77176!2sLimak%20Skopje%20Project!5e0!3m2!1str!2str!4v1686838648705!5m2!1str!2str"
             width="600"
